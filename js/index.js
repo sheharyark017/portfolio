d8f3cfe797d09ce9d84f8af2ee0d05245a3609d1
sheharyark017 <sheharyark017@gmail.com>
2021-09-27 13:02:52 +0500

    js on nav links

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ const contact = document.querySelector("#contact");
 const aboutScrollBtn = document.querySelector(".about-scroll");
 const skillScrollBtn = document.querySelector(".skill-scroll");
 const constactScrollBtn = document.querySelector(".contact-scroll");
+const contactbtn = document.querySelector(".intro-contact-btn");
 
 aboutScrollBtn.addEventListener("click", function () {
   about.scrollIntoView({ behavior: "smooth" });
@@ -14,5 +15,24 @@ skillScrollBtn.addEventListener("click", function () {
 });
 
 constactScrollBtn.addEventListener("click", function () {
+  contact.scrollIntoView({ behavior: "smooth" });
+});
+
+document.querySelector(".nav-links").addEventListener("click", function (e) {
+  e.preventDefault();
+  // console.log(e.target);
+
+  // matching strategy
+  console.log(e.target);
+  if (e.target.classList.contains("nav-link")) {
+    const id = `${e.target.getAttribute("href")}`;
+    console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+contactbtn.addEventListener("click", function (e) {
+  e.preventDefault();
+
   contact.scrollIntoView({ behavior: "smooth" });
 });
