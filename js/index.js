9c70994199fa344465706b69b3696b32a1b030b9
sheharyark017 <sheharyark017@gmail.com>
2021-09-27 10:03:44 +0500

    home page incomplete

--- js/index.js
+++ js/index.js
@@ -1,7 +1,18 @@
 const about = document.querySelector("#about");
+const skills = document.querySelector("#skills");
+const contact = document.querySelector("#contact");
 const aboutScrollBtn = document.querySelector(".about-scroll");
-const squares = document.querySelectorAll(".square");
+const skillScrollBtn = document.querySelector(".skill-scroll");
+const constactScrollBtn = document.querySelector(".contact-scroll");
 
 aboutScrollBtn.addEventListener("click", function () {
   about.scrollIntoView({ behavior: "smooth" });
+});
+
+skillScrollBtn.addEventListener("click", function () {
+  skills.scrollIntoView({ behavior: "smooth" });
+});
+
+constactScrollBtn.addEventListener("click", function () {
+  contact.scrollIntoView({ behavior: "smooth" });
 });
